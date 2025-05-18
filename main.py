6095ceb77efa28fa44c074d75f9c7aa33e2b9d28
Naushad Ali <nashadali585@gmail.com>
2025-05-18 13:05:02 +0500

    Patient Management System

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+app= FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Patient Management System Api"}
+
+# About the API
+@app.get("/about")
+async def about():
+    return {
+        "message": "management system for patients for a hospital"
+    }
+
+
+
